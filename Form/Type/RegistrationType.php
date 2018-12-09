51aca6b3ffa0238c02f3e6e87bafa52ed5862dd4
Michał Kurcewicz <mkurc1@gmail.com>
2018-12-09 16:11:16 +0100

    Remove constraint from registration form

--- Form/Type/RegistrationType.php
+++ Form/Type/RegistrationType.php
@@ -9,7 +9,6 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use UserBundle\Entity\User;
 
 class RegistrationType extends AbstractType
